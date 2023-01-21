a2094e5e4b7001ae34b35e6fc60cab358092543a
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2023-01-21 01:04:24 -0800

    Add files via upload

--- Flights/WSJ Rankings.R
+++ Flights/WSJ Rankings.R
@@ -1,1 +1,149 @@
+##### Airline Rankings #####
+##### By: Stephan Teodosescu #####
+##### January 2023 #####
 
+library(tidyverse)
+library(magick)
+library(rvest) # for webscraping
+library(polite)
+library(gt) #for 538-themed tables
+library(gtExtras)
+library(glue)
+library(ggtext)
+library(gganimate)
+library(rlang)
+library(RCurl)
+library(ggimage) #for working with logos
+library(gtExtras)
+library(zoo)
+library(janitor)
+library(prismatic)
+library(patchwork)
+library(ggsci)
+library(rsvg)
+library(ggbump)
+
+
+
+#### Load data and Data preprocessing ##### 
+
+# Custom ggplot theme (inspired by Owen Phillips at the F5 substack blog)
+theme_custom <- function () { 
+    theme_minimal(base_size=11, base_family="Outfit") %+replace% 
+        theme(
+            panel.grid.minor = element_blank(),
+            plot.background = element_rect(fill = 'floralwhite', color = "floralwhite")
+        )
+}
+
+# Table theme
+gt_theme_538 <- function(data,...) {
+    data %>%
+        opt_table_font(
+            font = list(
+                google_font("Outfit"),
+                default_fonts()
+            )
+        ) %>%
+        tab_style(
+            style = cell_borders(
+                sides = "bottom", color = "transparent", weight = px(2)
+            ),
+            locations = cells_body(
+                columns = TRUE,
+                # This is a relatively sneaky way of changing the bottom border
+                # Regardless of data size
+                rows = nrow(data$`_data`)
+            )
+        )  %>% 
+        tab_options(
+            column_labels.background.color = "white",
+            table.border.top.width = px(3),
+            table.border.top.color = "transparent",
+            table.border.bottom.color = "transparent",
+            table.border.bottom.width = px(3),
+            column_labels.border.top.width = px(3),
+            column_labels.border.top.color = "transparent",
+            column_labels.border.bottom.width = px(3),
+            column_labels.border.bottom.color = "black",
+            data_row.padding = px(3),
+            source_notes.font.size = 12,
+            table.font.size = 16,
+            heading.align = "left",
+            ...
+        ) 
+}
+
+# create aspect ration to use throughout
+asp_ratio <- 1.618
+
+# Function for plot with logo generation
+add_logo <- function(plot_path, logo_path, logo_position, logo_scale = 10){
+    
+    # Requires magick R Package https://github.com/ropensci/magick
+    
+    # Useful error message for logo position
+    if (!logo_position %in% c("top right", "top left", "bottom right", "bottom left")) {
+        stop("Error Message: Uh oh! Logo Position not recognized\n  Try: logo_positon = 'top left', 'top right', 'bottom left', or 'bottom right'")
+    }
+    
+    # read in raw images
+    plot <- magick::image_read(plot_path)
+    logo_raw <- magick::image_read(logo_path)
+    
+    # get dimensions of plot for scaling
+    plot_height <- magick::image_info(plot)$height
+    plot_width <- magick::image_info(plot)$width
+    
+    # default scale to 1/10th width of plot
+    # Can change with logo_scale
+    logo <- magick::image_scale(logo_raw, as.character(plot_width/logo_scale))
+    
+    # Get width of logo
+    logo_width <- magick::image_info(logo)$width
+    logo_height <- magick::image_info(logo)$height
+    
+    # Set position of logo
+    # Position starts at 0,0 at top left
+    # Using 0.01 for 1% - aesthetic padding
+    
+    if (logo_position == "top right") {
+        x_pos = plot_width - logo_width - 0.01 * plot_width
+        y_pos = 0.01 * plot_height
+    } else if (logo_position == "top left") {
+        x_pos = 0.01 * plot_width
+        y_pos = 0.01 * plot_height
+    } else if (logo_position == "bottom right") {
+        x_pos = plot_width - logo_width - 0.01 * plot_width
+        y_pos = plot_height - logo_height - 0.01 * plot_height
+    } else if (logo_position == "bottom left") {
+        x_pos = 0.01 * plot_width
+        y_pos = plot_height - logo_height - 0.01 * plot_height
+    }
+    
+    # Compose the actual overlay
+    magick::image_composite(plot, logo, offset = paste0("+", x_pos, "+", y_pos))
+    
+}
+
+
+##### Data Preparation #####
+
+# Define colors to be used in viz
+airline_colors <- c(
+    "Delta" = "#012169",
+    "Alaska" = "#DD0000",
+    "Southwest" = "#F1BF00",
+    "United" = "#002654",
+    "Allegiant" = "#008C45",
+    "American" = "#C8102E",
+    "Spirit" = "#BA0C2F",
+    "Frontier" = "#F36C21",
+    "JetBlue" = "#046A38"
+)
+
+description_color <- "grey40"
+
+# read in airline ranking data from github
+
+airline_rankings <- read_csv("")
